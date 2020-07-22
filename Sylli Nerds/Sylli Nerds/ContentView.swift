a5988cb6254b901df4aa84394b10aa7093250622
Ryan Garland <ryangarland211@gmail.com>
2020-07-21 22:14:09 -0400

    App Gui Stuff

---
+++ Sylli Nerds/Sylli Nerds/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Sylli Nerds
+//
+//  Created by Ryan Garland on 7/21/20.
+//  Copyright © 2020 Ryan Garland. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
